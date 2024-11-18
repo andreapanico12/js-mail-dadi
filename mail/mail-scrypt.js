7b4303925649cf8ec082b0d374bfcf10b2a7a329
andreapanico12 <andreapanico12@gmail.com>
2024-11-18 16:20:55 +0100

    feat: add in mail-scrypt instrusctions and description of mail exercise.

---
+++ mail/mail-scrypt.js
@@ -0,0 +1,17 @@
+// =========== CONSEGNA=============
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Non è necessario provvedere alla validazione delle email.
+
+// ======SVOLGIMENTO========
+
+// 1) Identifico prima di tutto le variabili, l'array con la lista mail, la mail derivante dal prompt ( senza definirla), una variabile per il controllo della validità del dato nel prompt e una variabile messaggio.
+
+// 2) la mia logica prevede prima l'utilizzo di un ciclo while per verificare che l'input del prompt sia effettivamente una mail. Per farlo, all'interno del ciclo while, subito dopo il prompt che definisce la mail, metto una condizione (if) che garantisce la presenza del carattere "@" all'interno dell'input. Al termine del ciclo while verrà così ridefinito il valore della variabile che permette la chiusura del ciclo.
+
+// 3) per rispettare infine la consegna creo una condizione (if) che verifica l'appartenenza della mail definita con il prompt all'array della mail list. Verrà poi generato un messaggio per definire l'esito dell'accesso, anche in caso di input sbagliato.
+
+
+
+
+
+
