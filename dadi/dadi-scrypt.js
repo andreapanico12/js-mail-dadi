7432030784d3effc23c17553ad2798eecae22dfb
andreapanico12 <andreapanico12@gmail.com>
2024-11-18 16:43:07 +0100

    feat: add if cicle in dadi-scrypt with exercise solution

--- dadi/dadi-scrypt.js
+++ dadi/dadi-scrypt.js
@@ -10,14 +10,32 @@
 
 // 2)vado poi a definire una condizione (if) che confronti i due risultati e per ogni outcome dare un valore alla variabile messaggio diverso. Le condizioni saranno dunque : dado giocatore > dado computer e viceversa. Ho previsto anche un messaggio in caso di pareggio.
 
+// 3) Ho aggiunto infine un prompt iniziale per avviare il gioco, non particolarmente utile ma mi sembrava carino. 
 
 
+
+
+let comando = prompt(`Per giocare scrivi "tira"`)
 let message
+if (comando.includes(`tira`)){
+  let dadoGiocatore = Math.floor(Math.random() * 6) + 1;
+  console.log(`Questo è il tuo tiro: ${dadoGiocatore}`);
+  let dadoComputer = Math.floor(Math.random() * 6) + 1;
+  console.log(`Questo è il mio tiro: ${dadoComputer}`);
 
-let dadoGiocatore = Math.floor(Math.random() * 6) + 1;
-console.log(`Questo è il tuo tiro: ${dadoGiocatore}`);
-let dadoComputer = Math.floor(Math.random() * 6) + 1;
-console.log(`Questo è il mio tiro: ${dadoComputer}`);
+  if (dadoGiocatore > dadoComputer){
+  message = `${dadoGiocatore} è maggiore di ${dadoComputer}! Congratulazioni! Hai avuto solo fortuna!`
+  } else if (dadoComputer > dadoGiocatore){
+  message = `${dadoComputer} è maggiore di ${dadoGiocatore} Ahahahah ho vinto io!`
+  } else{
+  message = `Ah! Un pareggio! Giochiamo ancora!`
+  }
+  console.log(message)
+
+} else {
+  message = `Le regole le faccio IO! Scrivi tira per giocare!`
+  console.log(message)
+}
 
 
 
